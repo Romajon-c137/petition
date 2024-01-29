284777621b5f37474efbb8cfc8c785fe49cee41f
Romajon-c137 <gulamov.c137@gmail.com>
2024-01-29 16:16:41 +0600

    finalll

--- src/components/RandomSignature.jsx
+++ src/components/RandomSignature.jsx
@@ -1,35 +1,9 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { memo } from 'react'
+const RandomSignature = () => {
+	const randomIndex = Math.floor(Math.random() * 4) + 1
+	const imageUrl = require(`../assets/image/signature${randomIndex}.jpg`)
 
-const RandomSignature = ({ onImagesLoad }) => {
-  const [imagesLoaded, setImagesLoaded] = useState(false);
+	return <img src={imageUrl} alt='Random Signature' style={{ width: '100%' }} />
+}
 
-  useEffect(() => {
-    const handleImageLoad = () => {
-      const allImagesLoaded = document
-        .querySelectorAll('.random-signature img')
-        .every((img) => img.complete);
-
-      if (allImagesLoaded) {
-        setImagesLoaded(true);
-        onImagesLoad(); // Вызываем колбэк, чтобы сообщить родительскому компоненту о загрузке изображений
-      }
-    };
-
-    document.querySelectorAll('.random-signature img').forEach((img) => {
-      img.addEventListener('load', handleImageLoad);
-    });
-
-    return () => {
-      document.querySelectorAll('.random-signature img').forEach((img) => {
-        img.removeEventListener('load', handleImageLoad);
-      });
-    };
-  }, [onImagesLoad]); // Добавили onImagesLoad в зависимости, чтобы useEffect следил за его изменениями
-
-  const randomIndex = Math.floor(Math.random() * 4) + 1;
-  const imageUrl = require(`../assets/image/signature${randomIndex}.jpg`);
-
-  return <img className="random-signature" src={imageUrl} alt="Random Signature" style={{ width: '100%' }} />;
-};
-
-export default memo(RandomSignature);
+export default memo(RandomSignature)
