be52cb0f06caaf22e5fca5df275c472bec3857bc
Romajon-c137 <gulamov.c137@gmail.com>
2024-01-30 17:24:41 +0600

    SelectChangeEvent

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ import RefreshIcon from '@mui/icons-material/Refresh'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Select from '@mui/material/Select'
 
 import './main.css'
 import TodaysDate from './TodaysDate'
