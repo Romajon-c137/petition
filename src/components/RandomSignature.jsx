484edbfaaa119c755a15a4a71af44e40b9d9a2ad
Romajon-c137 <gulamov.c137@gmail.com>
2024-02-02 11:57:59 +0600

    random to 15

--- src/components/RandomSignature.jsx
+++ src/components/RandomSignature.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 const RandomSignature = () => {
-	const randomIndex = Math.floor(Math.random() * 4) + 1
+	const randomIndex = Math.floor(Math.random() * 15) + 1
 	const imageUrl = require(`../assets/image/signature${randomIndex}.jpg`)
 
 	return <img src={imageUrl} alt='Random Signature' style={{ width: '100%' }} />
